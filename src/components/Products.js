8bbb462e43490a564ad0d2f4e8ae4fc920504b4c
Adam Toczko <adamtoczko80@gmail.com>
2020-02-08 16:20:43 +0100

    completed products component and prepeared for adding to basket

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react'
 
 export default class Products extends Component {
-
+    
     render() {
+        
         const productItems = this.props.products.map(product =>
+            
             <div className="col-md-4">
                 <div className="thumbnail text-center" >
-                    <a href={`#${product.id}`} onClick={this.props.handleAddToCard}>
+                    <a href={`#${product.id}`} onClick={(event)=> this.props.handleAddToCard(event,product)}>
                         <img src={`/products/${product.sku}_2.jpg`} alt={product.title} />
                         <p>
                             {product.title}
                         </p>
-                    
                     </a>
+                    <div>
+                        <b>$ {product.price} </b>
+                        <button className="btn btn-primary btn-sm"
+                        onClick={(event)=> this.props.handleAddToCard(event,product)}
+                        > Add To Cart 
+                        </button>
+                    </div>
                 </div>
             </div>
         )
-
         return (
             <div className="row">
                 {productItems}
